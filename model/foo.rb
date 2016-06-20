9f59041431d73f604386e696b762c44adb0be21c
juliani2 <julianezequielmoreno@gmail.com>
2016-06-20 20:07:22 -0300

    preparando el repo 2: agrego test foo

---
+++ model/foo.rb
@@ -0,0 +1,5 @@
+class Foo
+	def do_foo
+		"foo"
+	end
+end
---
+++ spec/foo_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative '../model/foo'
+
+describe 'Foo' do
+
+  it 'deberia retornar foo cuando hago do_foo' do
+    foo = Foo.new
+    expect(foo.do_foo).to eq "foo"
+  end
+end
