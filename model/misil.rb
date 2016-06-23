42c66ade5b71048f6697ec701c4088eeaa033d7b
juliani2 <julianezequielmoreno@gmail.com>
2016-06-23 16:24:11 -0300

    deberia retornar 0 cuando hago chocar_con(misil)

--- model/misil.rb
+++ model/misil.rb
@@ -2,6 +2,14 @@ require_relative '../model/objeto_espacial'
 
 class Misil < ObjetoEspacial
 
+  def mapa_efectos
+    @hash_map = {"misil" => EfectoDestructivo.new(100), "nave" => EfectoDestructivo.new(100)}
+  end
+
+  def chocar_con(objeto_espacial)
+  	 @hash_map[objeto_espacial.obtener_nombre].ejecutar_efecto(@vida)
+  end
+
   def obtener_nombre
     return "misil"
   end
--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -12,4 +12,12 @@ describe 'Misil' do
     misil = Misil.new(100,100)
     expect(misil.obtener_masa).to eq 100
   end
+
+  it 'deberia retornar 0 cuando hago chocar_con(misil)' do
+    misil = Misil.new(100,100)
+    misil1 = Misil.new(100,100)
+    misil.mapa_efectos
+    expect(misil.chocar_con(misil1)).to eq 0
+  end
+
 end
