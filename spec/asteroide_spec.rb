ccaee9c2010686a7a8853826dc2f6c6b76ce4943
juliani2 <julianezequielmoreno@gmail.com>
2016-06-23 20:34:12 -0300

    deberia retornar 100 cuando hago obtener_masa de un asteroide nuevo

--- spec/asteroide_spec.rb
+++ spec/asteroide_spec.rb
@@ -3,8 +3,14 @@ require_relative '../model/asteroide'
 require_relative '../model/objeto_espacial'
 
 describe 'Asteroide' do
+
   it 'deberia retornar 100 cuando hago obtener_vida de un asteoroide nuevo' do
     asteroide = Asteroide.new(100, 0)
     expect(asteroide.obtener_vida).to eq 100
+  end
+
+  it 'deberia retornar 100 cuando hago obtener_masa de un asteroide nuevo' do
+    asteroide = Asteroide.new(100,100)
+    expect(asteroide.obtener_masa).to eq 100
   end
 end
