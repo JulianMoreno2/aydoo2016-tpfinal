59a6fc094161aba6dbe0141ddcf47bd25927478d
juliani2 <julianezequielmoreno@gmail.com>
2016-06-23 16:10:52 -0300

    deberia retornar 100 cuando hago obtener_masa de un misil nuevo

--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -7,4 +7,9 @@ describe 'Misil' do
     misil = Misil.new(100, 0)
     expect(misil.obtener_vida).to eq 100
   end
+
+  it 'deberia retornar 100 cuando hago obtener_masa de un misil nuevo' do
+    misil = Misil.new(100,100)
+    expect(misil.obtener_masa).to eq 100
+  end
 end
