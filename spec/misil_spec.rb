2d2c76f4f52d53b757031699b2ff623394d22818
juliani2 <julianezequielmoreno@gmail.com>
2016-06-23 16:09:32 -0300

    deberia retornar 100 cuando hago obtener_vida de un misil nuevo

---
+++ spec/misil_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative '../model/misil'
+require_relative '../model/objeto_espacial'
+
+describe 'Misil' do
+  it 'deberia retornar 100 cuando hago obtener_vida de un misil nuevo' do
+    misil = Misil.new(100, 0)
+    expect(misil.obtener_vida).to eq 100
+  end
+end
