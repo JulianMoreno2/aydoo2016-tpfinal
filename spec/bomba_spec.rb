ad075de2569bf168fb063dfa3f4429b00573eeec
juliani2 <julianezequielmoreno@gmail.com>
2016-06-23 16:40:16 -0300

    deberia retornar 100 cuando hago obtener_masa de una bomba nueva

--- spec/bomba_spec.rb
+++ spec/bomba_spec.rb
@@ -7,4 +7,9 @@ describe 'Bomba' do
     bomba = Bomba.new(100, 0)
     expect(bomba.obtener_vida).to eq 100
   end
+  
+  it 'deberia retornar 100 cuando hago obtener_masa de una bomba nueva' do
+    bomba = Bomba.new(100,100)
+    expect(bomba.obtener_masa).to eq 100
+  end
 end
