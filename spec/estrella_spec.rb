c42c1eca0709ee530a6834e56aa998e186379a3e
juliani2 <julianezequielmoreno@gmail.com>
2016-06-24 00:23:04 -0300

    deberia retornar 100 cuando hago obtener_masa de una estrella nueva

--- spec/estrella_spec.rb
+++ spec/estrella_spec.rb
@@ -3,8 +3,14 @@ require_relative '../model/estrella'
 require_relative '../model/objeto_espacial'
 
 describe 'Estrella' do
+  
   it 'deberia retornar 100 cuando hago obtener_vida de una estrella nueva' do
     estrella = Estrella.new(100, 0)
     expect(estrella.obtener_vida).to eq 100
+  end
+  
+  it 'deberia retornar 100 cuando hago obtener_masa de una estrella nueva' do
+    estrella = Estrella.new(100,100)
+    expect(estrella.obtener_masa).to eq 100
   end
 end
