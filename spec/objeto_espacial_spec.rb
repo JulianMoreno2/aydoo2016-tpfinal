1fee4904884c771bd16a74db8f2fa9fe873216fe
juliani2 <julianezequielmoreno@gmail.com>
2016-06-24 20:44:07 -0300

    deberia retornar 100 cuando hago obtener_masa de un objeto espacial

--- spec/objeto_espacial_spec.rb
+++ spec/objeto_espacial_spec.rb
@@ -6,5 +6,9 @@ describe 'ObjetoEspacial' do
     objEspacial = ObjetoEspacial.new(100, 100)
     expect(objEspacial.obtener_vida).to eq 100
   end
-
+    
+  it 'deberia retornar 100 cuando hago obtener_masa de un objeto espacial' do
+    objEspacial = ObjetoEspacial.new(100,100)
+    expect(objEspacial.obtener_masa).to eq 100
+  end
 end
