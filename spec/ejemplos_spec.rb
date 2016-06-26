6bd8665f9c76c715654d71ce7141dbe8678dcf31
juliani2 <julianezequielmoreno@gmail.com>
2016-06-25 21:46:19 -0300

    Una Nave (vida=100, masa=100) choca con un Asteriode (vida=50, masa=100) expect Asteroide (vida=50, masa=110)

--- spec/ejemplos_spec.rb
+++ spec/ejemplos_spec.rb
@@ -10,4 +10,12 @@ describe 'Ejemplos' do
     nave.chocar_con(asteroide)
     expect(nave.obtener_masa).to eq 50
   end
+
+  it 'Una Nave (vida=100, masa=100) choca con un Asteriode (vida=50, masa=100)' do
+    nave = Nave.new
+    asteroide = Asteroide.new
+    asteroide.modificar_vida(50)
+    nave.chocar_con(asteroide)
+    expect(asteroide.obtener_masa).to eq 110
+  end
 end
